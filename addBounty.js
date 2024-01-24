176714f0a1a33b0377b6e1d20f81809f66f35e68
Raul Moldovan <raul.mldv99@gmail.com>
2024-01-24 21:34:25 +0200

    Add bounty on issue workflow

---
+++ addBounty.js
@@ -0,0 +1,27 @@
+const SmartContribute = artifacts.require('./contracts/SmartContribute.sol');
+const Web3 = require('web3');
+const web3 = new Web3();
+
+const githubUser = process.env.GITHUB_USER
+const issueId = process.env.GITHUB_ISSUE_HREF
+const bounty = process.env.GITHUB_ISSUE_BOUNTY
+
+const ethMul = BigInt("1000000000000000000");
+
+module.exports = async function (callback) {
+  try {
+    // Change to remote
+    const smartContribute = await SmartContribute.deployed();
+
+    const bountyWei = web3.utils.toWei(bounty, 'ether');
+    let ethBounty = BigInt(bountyWei);
+    console.log('Bounty:', ethBounty.toString());
+
+    const result = await smartContribute.registerIssue(issueId, githubUser, ethBounty.toString());
+    console.log('Register Issue Result:', result);
+    callback();
+  } catch (error) {
+    console.error('Error:', error);
+    callback(error);
+  }
+};
